384a8cbe74ab07cebb569fbd733de8e95797746e
Bladedkiwi <bladedkiwi@byui.edu>
2021-06-13 11:34:08 -0500

    Development Build

--- reunion/src/app/app.component.ts
+++ reunion/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hyfr-root',
+  selector: 'hfr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hyfr';
+  title = 'reunion';
 }
